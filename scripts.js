29ff1556d6910e9c52fe5abcbe4a8999e19bafd6
Juan Fernando Donoso <55159143+juanfdonoso@users.noreply.github.com>
2020-02-19 10:21:44 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
-function mostrarRegistro(){
+function mostrarRegistro(id){
+    document.location.assign("index.php?id="+id);
+}
+
+function mostrarResultados(letraEscogida){
+    document.location.assign("index.php?letra="+letraEscogida);
+}
+
+function mostrarDatosIndividuales(){
     document.getElementById("contenedor").style.display = "none";
     document.getElementById("registro").style.display = "flex";
 }
 
-function mostrarResultados(){
-    document.getElementById("contenedor").style.display = "block";
-    document.getElementById("registro").style.display = "none";
+function ocultarTarjeton(){
+    //recarga la página
+    document.location.assign("index.php");
 }
