1fd87b2ebaf6c8b9208099297ed6e7cba6922708
Juan Fernando Donoso <55159143+juanfdonoso@users.noreply.github.com>
2020-03-04 09:55:11 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -7,53 +7,206 @@
     <title>Directorio</title>
     <link rel="stylesheet" type="text/css" href="estilos.css">
     <script src="https://kit.fontawesome.com/5637dd924f.js" crossorigin="anonymous"></script>
+    <script src="./scripts.js"></script> 
 </head>
 <body>
 <div class="header">
 <h1>Directorio</h1>
-<button type="button">Nuevo Contacto</button>
+<button type="button" onClick="abrirModal()">Nuevo Contacto</button>
 </div>
+
+<?php 
+include "conexion.php";
+?>
 
 <section class="botonera">
     <?php
-        echo "<button type='button'>A</button>";
-        echo "<button type='button'>B</button>";
+
+        for ($i=65; $i<=90; $i++){
+            echo "<button type='button' onClick=mostrarResultados('".chr($i)."')>".chr($i)."</button>";
+        }
     ?>
 </section>
 
 <section class="busquedas">
-    <form method="post" action="">
-        <input type="text" class="campo" />
-        <button type="submit" class="boton" onClick="mostrarResultados()"><i class="fas fa-search"></i></button>
+    <form method="post" action="index.php">
+        <input type="text" class="campo" name="busqueda"/>
+        <button type="submit" class="boton"><i class="fas fa-search"></i></button>
     </form>
 </section>
 
+<?php
+    //checamos si se ha enviado un querystring a la página o el formulario con una búsqueda
+    if (isset($_REQUEST["letra"])){
+        $letraParaBuscar = $_REQUEST["letra"];
+
+        //buscamos los apellidos que inician con la letra seleccionada
+        $sql = "select idDirectorio, nombre, apellido from juanf_directorio where apellido like '".$letraParaBuscar."%' and activo = 1 order by apellido";
+        $rs = ejecutar($sql);
+
+    }else if (isset($_POST["busqueda"])){
+        $registroParaBuscar = $_POST["busqueda"];
+
+        $sql = "select idDirectorio, nombre, apellido from juanf_directorio where apellido like '%".$registroParaBuscar."%' order by apellido";
+        $rs = ejecutar($sql);
+    }
+?>
+
 <section class="listaResultados">
     <div class = "contenedor" id="contenedor">
-        <div id="r1">Registros encontrados: </div>
+        <?php
+        if (isset($_REQUEST["letra"]) || isset($_POST["busqueda"])){
+            //checamos si hemos insertado un nuevo registro, para imprimir el mensaje
+            //correspondiente
+            if (isset($_REQUEST["accion"])){
+                if ($_REQUEST["accion"]=="ingresar"){
+                    echo '<div id="r1">Se insertó correctamente el registro en la BD </div>';
+                }
+            }else{
+                echo '<div id="r1">Registros encontrados: </div>';
+            }
+            
+            echo '<ul class="listaNombres">';
+
+            //checamos si la búsqueda realizada encontró registros en la BD
+             if (mysqli_num_rows($rs) != 0){
+                $k = 0;
+                while ($datos = mysqli_fetch_array($rs)){
+                    if ($k % 2 == 0){
+                        echo "<li class='oscuro'>";
+                    }else{
+                        echo "<li class='claro'>";
+                    }
+                    echo "<a href='javascript:mostrarRegistro(".$datos['idDirectorio'].")'>".$datos["apellido"]."</a>, ".$datos["nombre"]."</li>";
+                    $k++;
+                }
+            }else{
+                echo 'No se encontraron registros con la letra '.$letraParaBuscar;
+            }
+
+            echo "</ul>";
+
+        }else if (isset($_REQUEST["id"])){
+            $id = $_REQUEST["id"];
+            $sql = "select * from juanf_directorio where idDirectorio =".$id;
+            $rs = ejecutar($sql);
+            $registro = mysqli_fetch_array($rs);
+            
+           
+
+        } else {
+            if (isset($_REQUEST["borrar"])){
+                if ($_REQUEST["borrar"] == "yes"){
+                    echo '<div class="mensajeBorrar">El registro fue borrado de la base de datos</div>';
+                }
+            }
+
+
+            echo '<div id="r1">Seleccione una letra o realize una búsqueda para desplegar los registros del directorio</div>';
+        }
+        ?>  
         
-        <ul class="listaNombres">
-            <li class="oscuro">Donoso, Juan</li>
-            <li class="claro">Donoso, Jose</li>
-            <li class="oscuro">Donoso, Juan Fernando</li>
-        </ul>
     </div>
 
-    <div class="contenedorRegistro" id="registro"> 
-        <button type="button"><i class="fas fa-caret-square-left"></i></button>
-        <div class="registro"></div>
-        <button type="button"><i class="fas fa-caret-square-right"></i></button>
-    </div>
+
+    <?php
+    if (isset($_REQUEST["id"])){
+    ?>
+    
+        <div class="contenedorRegistro" id="registro"> 
+            <button type="button"><i class="fas fa-caret-square-left"></i></button>
+            <div class="registro">
+                <div class="cerrar">
+                    <button type="button" onClick="editarRegistro(<?php echo $id; ?>)"><i class="fas fa-edit"></i></button>
+                    <button type="button" onClick="borrarRegistro(<?php echo $id; ?>)"><i class="fas fa-trash"></i></button>
+                    <button type="button" onClick="cerrarTarjeton()"><i class="fas fa-window-close"></i></button>
+                </div>
+
+                <div class="nombre"><?php echo $registro["nombre"]." ".$registro["apellido"];?> </div>
+                
+                <div class="icono"><i class="fas fa-envelope"></i></div>
+                <div class="datos"><?php echo $registro["email"];?> </div>
+                <div class="foto">
+                    <?php if ($registro["foto"] == null) {
+                        echo "<img src='fotos/noFoto.png' class='fotoContacto'>";
+                    }
+                    ?>
+                </div>
+
+                <div class="icono"><i class="fas fa-building"></i></div>
+                <div class="datos"><?php echo $registro["empresa"];?> </div>
+
+                <div class="icono"><i class="fas fa-phone"></i></div>
+                <div class="datos"><?php echo $registro["telefono"];?> </div>
+                
+                <div class="icono"><i class="fas fa-file-alt"></i></div>
+                <div class="datos"><?php echo $registro["comentarios"];?> </div>
+
+                <div class="sinIcono"></div>
+                <div class="mensaje">
+                    <?php
+                        if (isset($_REQUEST["accion"])){
+                            if ($_REQUEST["accion"]=="editar"){
+                                echo "El registro fue actualizado";
+                            }
+                        }
+                    ?>
+                </div>
+
+                
+
+            </div>
+            <button type="button"><i class="fas fa-caret-square-right"></i></button>
+        </div>
+        <script language="javascript"> mostrarDatosRegistro()</script>
+    <?php
+    }
+    ?>
 
 </section>
 
-<div class="modal">
+<div class="modal" id="vModal">
     <div class="modal-bg">
-        <div class="modal-container"> </div>
+        <form method = "post" action = "index_xt.php" id="f2">
+        <div class="modal-container"> 
+            <div class="cerrar">
+                <button type="button" onClick="cerrarModal()"><i class="fas fa-window-close"></i></button>
+            </div>
+            <div class="titulo">Ingreso Nuevo Registro</div>
+
+            
+            <div class="iconos"><i class="fas fa-user"></i></div>
+            <div class="formulario"><input type="text" placeholder="nombre" name="nombre" class="camposModal" id="c1"/></div>
+
+            <div class="iconos"><i class="fas fa-user"></i></div>
+            <div class="formulario"><input type="text" placeholder="apellido" name="apellido" class="camposModal" id="c2"/></div>
+
+            <div class="iconos"><i class="fas fa-building"></i></div>
+            <div class="formulario"><input type="text" placeholder="empresa" name="empresa" class="camposModal" id="c3"/></div>
+
+            <div class="iconos"><i class="fas fa-envelope"></i></div>
+            <div class="formulario"><input type="text" placeholder="email" name="email" class="camposModal" id="c4"/></div>
+
+            <div class="iconos"><i class="fas fa-phone"></i></div>
+            <div class="formulario"><input type="text" placeholder="telefono" name="telefono" class="camposModal"/></div>
+
+            <div class="iconos"><i class="fas fa-comment"></i></div>
+            <div class="formulario"><textarea name="comentarios" rows="5" cols="40"></textarea></div>
+
+            <div class="iconos"></div>
+            <div class="formulario"><button type="button" class="botonFormularioModal" onClick="validarFormulario()">Ingresar</button>
+
+            
+            <div class="iconos"></div>
+            <div class="formulario"><span id="msj" class="mensaje"></span></div>
+
+
+        </div>
+        </form>
     </div>
 </div>
 
 
-<script src="./scripts.js"></script>  
+ 
 </body>
 </html>
